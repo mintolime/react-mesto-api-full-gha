df9f1c4252fd7bda1c840f610122accb94bd319a
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-05-11 10:14:38 +0700

    dev : added send email on backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -24,7 +24,7 @@ const login = (req, res, next) => {
         // .cookie('jwt', token, {
         //   httpOnly: true,
         // })
-        .send({ token });
+        .send({ token, email });
     })
     .catch(() => { next(new UnauthorizedError('Необходима авторизация')); });
 };
